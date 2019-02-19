efeebcd2142a24dc88620baf4bb114cfffa47a9c
Johanes Anggara <johanes.p.a.s@gmail.com>
2019-02-20 00:48:27 +0700

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,60 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// const user = {name: 'Johanes', age: 25};
+// const {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true}, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c6abf8ed100c26d0c3e76e0')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Users').find({
+    //     name: 'Mike'
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) =>{
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) =>{
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) =>{
+    //     console.log(result);
+    // });
+
+    // db.collection('Users').deleteMany({name: 'Johanes Anggara'}).then((result) =>{
+    //     console.log(result);
+    // });
+
+    db.collection('Users').deleteOne({_id: new ObjectID('5c6b103e9175e172318c0d1e')}).then((result) =>{
+        console.log(result);
+    });
+
+    // client.close();
+});
